0bb78882bddf9404d99df2972f355d8143100167
Jason <jay.bourner@googlemail.com>
2025-06-21 13:15:27 +0000

    testing out vite features and config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,31 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-const isProduction = process.env.NODE_ENV == 'production';
+// const isProduction = process.env.NODE_ENV == 'production';
+
+// if(process.env.NODE_ENV === 'production') {
+//     console.log('Production build');
+// } else {
+//     console.log('Development build');
+// }
+
+let isProduction = process.env.NODE_ENV;
+
+const displayMode = () => {
+    console.log('development mode:', isProduction);
+}
 
 export default defineConfig({
     build: {
-        sourcemap: true,
+        MODE: 'production',
     },
+    // mode: process.env.NODE_ENV || 'development',
     plugins: [
         laravel({
             input: ['resources/js/app.js'],
             refresh: true,
         }),
+        displayMode(),
     ],
 });
+
